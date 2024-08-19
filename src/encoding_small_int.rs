dff937b4e192aa15cb914ba017fc9386a62f4079
Ben England <benengland82@gmail.com>
2024-08-19 11:56:31 -0400

    transform append/read_uint32_len into separate implementation of trait create an object to represent trait implementation embed bitstring in this object allow object to operate on pre-existing bitstring, export bitstring improve testing

--- src/encoding_small_int.rs
+++ src/encoding_small_int.rs
@@ -1,6 +1,6 @@
 use bitstring::BitString;
 use crate::dyn_bit_string::DynBitString;
-use crate::encoding_uint_trait::{EncodingUint, UintEncoding};
+use crate::encoding_uint_trait::{BITSTRING_CONTINUE, BITSTRING_END, EncodingUint, UintEncoding};
 
 // encode very small integers using variable-length encoding
 
@@ -10,23 +10,34 @@ pub struct SmallIntEncoding {
 
 impl EncodingUint for SmallIntEncoding {
     fn new() -> Self {
-        let ec = UintEncoding { bstr: DynBitString::null() };
-        SmallIntEncoding { encoding: ec }
+        SmallIntEncoding { encoding: UintEncoding { bstr: DynBitString::null() }}
     }
 
-    fn get_encoding(&self) -> DynBitString {
+    fn get_bitstr_encoding(&self) -> DynBitString {
         self.encoding.bstr.clone()
     }
 
-    fn append_uint32(& mut self, len_in: u32) {
-        assert!(len_in < 8);  // FIXME: handle case where it doesn't fit in 3 bits
-        let mut v = len_in;
+    fn from_bitstr_encoding(bs : DynBitString) -> Self {
+        SmallIntEncoding { encoding: UintEncoding { bstr: bs }}
+    }
+    fn append_uint32(& mut self, v_in: u32) {
+        assert!(v_in < (1 << 4));  // FIXME: handle case where it doesn't fit in 4 bits
+        let mut v = v_in;
         let bs = &mut  self.encoding.bstr;
-        for _k in 0..3 { // from least significant to most significant
-            let next_bit = (v & 1) != 0;
-            bs.append(next_bit);
-            v >>= 1;
+        let first_bit = (v & 1) != 0;
+        bs.append(first_bit);
+        v >>= 1;
+        if v == 0 {
+            bs.append(BITSTRING_END);
+        } else {
+            bs.append(BITSTRING_CONTINUE);
+            for _k in 0..3 { // from least significant to most significant
+                let next_bit = (v & 1) != 0;
+                bs.append(next_bit);
+                v >>= 1;
+            }
         }
+        assert_eq!(v, 0);
     }
 
     // inverse of append_uint32_len
@@ -35,13 +46,22 @@ impl EncodingUint for SmallIntEncoding {
     fn read_uint32(&self, bitstring_cursor: &mut usize) -> u32 {
         let mut v = 0;
         let mut bitmask = 1;
-        let bs = self.get_encoding();
-        for _j in 0..3 {
-            if bs.get(*bitstring_cursor) {
-                v |= bitmask;
+        let bs = self.get_bitstr_encoding();
+        if bs.get(*bitstring_cursor) {
+            v |= bitmask;
+        }
+        *bitstring_cursor += 1;
+        bitmask <<= 1;
+        let continue_bit = bs.get(*bitstring_cursor);
+        *bitstring_cursor += 1;
+        if continue_bit == BITSTRING_CONTINUE {
+            for _j in 0..3 {
+                if bs.get(*bitstring_cursor) {
+                    v |= bitmask;
+                }
+                *bitstring_cursor += 1;
+                bitmask <<= 1;
             }
-            *bitstring_cursor += 1;
-            bitmask <<= 1;
         }
         v
     }
@@ -55,20 +75,31 @@ pub mod tests {
         use std::str::FromStr;
         let mut t = SmallIntEncoding::new();
         t.append_uint32(5);
-        let bs = t.get_encoding();
-        let expected_bs = DynBitString::from_str("b101").unwrap();
+        let bs = t.get_bitstr_encoding();
+        let expected_bs = DynBitString::from_str("b11010").unwrap();
+        assert_eq!(bs, expected_bs);
+    }
+
+    #[test]
+    pub fn test_append_uint32_0() {
+        use std::str::FromStr;
+        let mut t = SmallIntEncoding::new();
+        t.append_uint32(0);
+        let bs = t.get_bitstr_encoding();
+        let expected_bs = DynBitString::from_str("b00").unwrap();
         assert_eq!(bs, expected_bs);
     }
 
     #[test]
     pub fn test_read_uint32() {
         let mut t = SmallIntEncoding::new();
-        t.append_uint32(6);
-        t.append_uint32(7);
+        let v_in : [u32; 5] = [ 0, 1, 2, 6, 7 ];
+        for k in 0..5 {
+            t.append_uint32(v_in[k]);
+        }
         let mut cursor : usize = 0;
-        let v1 = t.read_uint32(& mut cursor);
-        let v2 = t.read_uint32(& mut cursor);
-        assert_eq!(v1, 6);
-        assert_eq!(v2, 7);
+        for i in 0..5 {
+            assert_eq!(t.read_uint32(&mut cursor), v_in[i]);
+        }
     }
 }
