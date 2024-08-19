90a05aba2d26ca001f1083cd2fbd1a626ab84cb3
Ben England <benengland82@gmail.com>
2024-08-19 11:51:59 -0400

    add BITSTR_CONTINUE + END constants used in implementations remove encode/decode functions, not needed by users add functions to allow bitstrings to be imported/exported from encoding objects

--- src/encoding_uint_trait.rs
+++ src/encoding_uint_trait.rs
@@ -2,27 +2,28 @@ use crate::dyn_bit_string::DynBitString;
 
 // encode/decode a sequence of unsigned integer values
 
+#[derive(Debug)]
 pub struct UintEncoding {
     pub bstr : DynBitString,
 }
+
+pub const BITSTRING_CONTINUE : bool = true;
+pub const BITSTRING_END : bool = false;
 
 pub trait EncodingUint {
     // create new instance
     fn new() -> Self;
 
     // get bitstring encoding
-    fn get_encoding(&self) -> DynBitString;
+    fn get_bitstr_encoding(&self) -> DynBitString;
+
+    // prepare to decode an encoded bitstring using read_
+    fn from_bitstr_encoding( bstr_in : DynBitString ) -> Self;
 
     // concatenate u32 encoding to a previously existing bit string
     fn append_uint32(&mut self, v_in : u32);
 
-    // generate bit string with just this u32 encoded in it
-    //fn encode_uint32(v_in : u32) -> DynBitString;
-
     // read the next u32 encoding from a bit string at the bit offset indicated by the cursor
     // cursor must be initialized to zero before using it
     fn read_uint32(&self, bitstring_cursor : & mut usize) -> u32;
-
-    // convert bitstring to a single u32 value (use if only 1 u32 encoding in the bitstring)
-    //fn decode_uint32(enc_len_val: &DynBitString) -> u32;
 }
