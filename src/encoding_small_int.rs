0e4de58420bcd631cfdbcc60f0259ccbc1305981
Ben England <benengland82@gmail.com>
2024-08-19 13:11:57 -0400

    make small_int handle from 0 thru 31 improve test cases

--- src/encoding_small_int.rs
+++ src/encoding_small_int.rs
@@ -2,7 +2,12 @@ use bitstring::BitString;
 use crate::dyn_bit_string::DynBitString;
 use crate::encoding_uint_trait::{BITSTRING_CONTINUE, BITSTRING_END, EncodingUint, UintEncoding};
 
-// encode very small integers using variable-length encoding
+// encode very small unsigned integers using variable-length encoding
+// this has to handle numbers up through 31, which is:
+// - maximum possible exponent-1
+// - theoretical upper bound on number of prime factors
+// encode starting from least significant bit to
+// most significant bit
 
 pub struct SmallIntEncoding {
     pub encoding : UintEncoding
@@ -20,8 +25,9 @@ impl EncodingUint for SmallIntEncoding {
     fn from_bitstr_encoding(bs : DynBitString) -> Self {
         SmallIntEncoding { encoding: UintEncoding { bstr: bs }}
     }
+
     fn append_uint32(& mut self, v_in: u32) {
-        assert!(v_in < (1 << 4));  // FIXME: handle case where it doesn't fit in 4 bits
+        assert!(v_in < (1 << 5));
         let mut v = v_in;
         let bs = &mut  self.encoding.bstr;
         let first_bit = (v & 1) != 0;
@@ -31,10 +37,20 @@ impl EncodingUint for SmallIntEncoding {
             bs.append(BITSTRING_END);
         } else {
             bs.append(BITSTRING_CONTINUE);
-            for _k in 0..3 { // from least significant to most significant
+            for _k in 0..2 { // from least significant to most significant
                 let next_bit = (v & 1) != 0;
                 bs.append(next_bit);
                 v >>= 1;
+            }
+            if v == 0 {
+                bs.append(BITSTRING_END);
+            } else {
+                bs.append(BITSTRING_CONTINUE);
+                for _k in 0..2 {
+                    let next_bit = (v & 1) != 0;
+                    bs.append(next_bit);
+                    v >>= 1;
+                }
             }
         }
         assert_eq!(v, 0);
@@ -55,12 +71,23 @@ impl EncodingUint for SmallIntEncoding {
         let continue_bit = bs.get(*bitstring_cursor);
         *bitstring_cursor += 1;
         if continue_bit == BITSTRING_CONTINUE {
-            for _j in 0..3 {
+            for _j in 0..2 {
                 if bs.get(*bitstring_cursor) {
                     v |= bitmask;
                 }
                 *bitstring_cursor += 1;
                 bitmask <<= 1;
+            }
+            let continue_bit2 = bs.get(*bitstring_cursor);
+            *bitstring_cursor += 1;
+            if continue_bit2 == BITSTRING_CONTINUE {
+                for _j in 0..2 {
+                    if bs.get(*bitstring_cursor) {
+                        v |= bitmask;
+                    }
+                    *bitstring_cursor += 1;
+                    bitmask <<= 1;
+                }
             }
         }
         v
@@ -73,21 +100,24 @@ pub mod tests {
     #[test]
     pub fn test_append_uint32() {
         use std::str::FromStr;
-        let mut t = SmallIntEncoding::new();
-        t.append_uint32(5);
-        let bs = t.get_bitstr_encoding();
-        let expected_bs = DynBitString::from_str("b11010").unwrap();
-        assert_eq!(bs, expected_bs);
-    }
+        let test_cases = vec![
+            (0, "b00"),
+            (1, "b10"),
+            (2, "b01100"),
+            (5, "b11010"),
+            (6, "b01110"),
+            (7, "b11110"),
+            (15,"b1111110"),
+            (16,"b0100101"),
+            (31,"b1111111") ];
 
-    #[test]
-    pub fn test_append_uint32_0() {
-        use std::str::FromStr;
-        let mut t = SmallIntEncoding::new();
-        t.append_uint32(0);
-        let bs = t.get_bitstr_encoding();
-        let expected_bs = DynBitString::from_str("b00").unwrap();
-        assert_eq!(bs, expected_bs);
+        for (v, enc_str) in test_cases {
+            let mut t = SmallIntEncoding::new();
+            t.append_uint32(v);
+            let bs = t.get_bitstr_encoding();
+            let expected_bs = DynBitString::from_str(enc_str).unwrap();
+            assert_eq!(bs, expected_bs);
+        }
     }
 
     #[test]
